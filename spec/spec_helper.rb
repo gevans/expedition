d71810fbb54dba4e202f41d330e06e80695efeda
Gabe Evans <gabe@ga.be>
2014-05-08 12:57:36 -0700

    Use UTC timezone in specs for consistent timestamp parsing across builds

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,15 @@ RSpec.configure do |config|
   config.order = 'random'
   config.run_all_when_everything_filtered = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  config.before(:suite) do
+    @time_zone = ENV['TZ']
+    ENV['TZ']  = 'UTC'
+  end
+
+  config.after(:suite) do
+    ENV['TZ'] = @time_zone
+  end
 end
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
